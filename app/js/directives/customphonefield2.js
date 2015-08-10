09c88332837e42a84a3c5810fca9475ea95b5096
Dave Baldwin <dave.baldwin@cenveo.com>
2015-08-10 09:24:31 -0500

    Removed Custom Mask Field and added Custom Phone Field 2.

---
+++ app/js/directives/customphonefield2.js
@@ -0,0 +1,27 @@
+four51.app.directive('customphonefield2', function() {
+    var obj = {
+        scope: {
+            spec : '=',
+            label: '@label',
+            mask: '@mask'
+        },
+        restrict: 'E',
+        templateUrl: 'partials/controls/customPhoneField2.html',
+        controller: ['$scope', function($scope) {
+            $scope.$watch('spec', function(n,o) {
+                if (n) $scope.phoneNumber2 = $scope.spec.Value;
+            });
+
+            $scope.$watch('phoneNumber2', function(value){
+                if (value) {
+                    $scope.spec.Value = value.substring(0, 3) + value.substring(3, 6) + value.substring(6, 10);
+				}
+                else if(value == ""){
+                    $scope.spec.Value = "";					
+                }
+            });
+        }]
+    };
+
+    return obj;
+});
